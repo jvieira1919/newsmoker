44e0b319a697c84c4d6412e294b4ddd11866e5bf
JV12 <jvieira1919@gmail.com>
2020-06-02 18:04:06 -0400

    updates

--- mysql.sql
+++ mysql.sql
@@ -7,7 +7,9 @@ create table department(
  name_dept varchar(30) not null);
  
  insert into department(name_dept)
- values("Foods", "Sports", "Health", "Fashions");
+ values("Foods"), ("Sports"), ("Health"), ("Fashions");
+ 
+ select * from department;
  
  create table roles(
  id int primary key auto_increment,
@@ -15,11 +17,21 @@ create table department(
  salary decimal (10,2) not null,
  department_id int);
  
+ insert into roles(title, salary, department_id)
+ values("Cook", 80000, 1), ("Director of Sports", 120000, 2), ("Nurse", 55000, 3), ("Public Relations", 100000, 4);
+ 
+ select * from roles;
+ 
  
  create table employees(
  id int primary key auto_increment,
  first_name varchar(30) not null,
  last_name varchar(30) not null,
- role_id int,
- manager_id int not null);
+ role_id int not null);
+
+ 
+ insert into employees(first_name, last_name, role_id)
+ values("Tom", "Jerry", 1), ("Bill", "Belichick", 2), ("Tom","Brady", 3), ("Kevin", "Ferg", 4);
+ 
+ select * from employees;
  
