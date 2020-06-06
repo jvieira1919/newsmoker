f8520e7d07bd9318033158345361a8b748f3ee25
JV12 <jvieira1919@gmail.com>
2020-06-05 23:11:42 -0400

    final updates

--- index.js
+++ index.js
@@ -2,160 +2,204 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "employee_tracker"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employees",
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
-    start();
+  if (err) throw err;
+  start();
 });
 
 function start() {
-    inquirer
-        .prompt({
-            type: "list",
-            message: "Locations",
-            name: "user_choice",
-            choices: ["Department", "Roles", "Employees", "Add-Department", "Add-Role", "Add-Employee", "Update-Role","EXIT"]
-        })
-        .then(function (answer) {
-            if (answer.user_choice === "Department") {
-                postDepartment();
-            } else if (answer.user_choice === "Roles") {
-                postRoles();
-            } else if (answer.user_choice === "Employees") {
-                postEmployees();
-            } else if(answer.user_choice === "Add-Department"){
-                addDepartment();
-            } else if (answer.user_choice === "Add-Role"){
-                addRole();
-            } else if (answer.user_choice === "Add-Employee"){
-                addEmployee();
-            } else if (answer.user_choice === "Update-Role"){
-                updateRole();
-            }else (answer.user_choice === "Exit")
-            connection.end();
-        })
-}
-
-function postDepartment() {
-    connection.query(" select * from department", function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        start();
-    });
-}
-
-function postRoles() {
-    connection.query(`select title, salary, name from roles
-     inner join department on roles.department_id=department.id`, function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        start();
-    });
-}
-
-function postEmployees() {
-    connection.query(`select first_name, last_name, title, salary, name from employees 
-    inner join role on employees.role_id=roles.id 
-    inner join department on roles.department_id=department.id`, function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        start();
+  inquirer
+    .prompt({
+      name: "userInput",
+      type: "list",
+      message: "Welcome would you like to view our directory?",
+      choices: [
+        "VIEW_DEPARTMENTS",
+        "VIEW_ROLES",
+        "VIEW_EMPLOYEES",
+        "ADD_DEPARTMENTS",
+        "ADD_ROLES",
+        "ADD_EMPLOYEES",
+        "UPDATE_EMPLOYEE_ROLE",
+        "EXIT",
+      ],
     })
+    .then((answer) => {
+      // based on their answer, either call the bid or the post functions
+      if (answer.userInput === "VIEW_DEPARTMENTS") {
+        viewDepartments();
+      } else if (answer.userInput === "VIEW_ROLES") {
+        viewRoles();
+      } else if (answer.userInput === "VIEW_EMPLOYEES") {
+        viewEmployees();
+      } else if (answer.userInput === "ADD_DEPARTMENTS") {
+        addDepartment();
+      } else if (answer.userInput === "ADD_ROLES") {
+        addRole();
+      } else if (answer.userInput === "ADD_EMPLOYEES") {
+        addEmployee();
+      } else if (answer.userInput === "UPDATE_EMPLOYEE_ROLE") {
+        update_Employee();
+      } else {
+        connection.end();
+      }
+    });
 }
 
-function printResults (err, result) {
+function viewDepartments() {
+  connection.query("SELECT * FROM department", (err, results) => {
     if (err) throw err;
-    console.log(result);
+    console.table(results);
     start();
+  });
 }
 
-async function addDepartment () {
-    const department = await inquirer.prompt([
-        {
-            name: "name",
-            message: "What is the name of the department"
-        }
-    ])
-    connection.query (`insert into department (name) values ('${department.name}')`, printResults )
+function viewRoles() {
+  connection.query(
+    `select title, salary, name from role 
+    inner join department on role.department_id=department.id`,
+    (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      start();
+    }
+  );
+}
+
+function viewEmployees() {
+  connection.query(
+    `select first_name, last_name, title, salary, name from employee 
+    inner join role on employee.role_id=role.id 
+    inner join department on role.department_id=department.id`,
+    (err, results) => {
+      if (err) throw err;
+      console.table(results);
+      start();
+    }
+  );
+}
+
+function printResults(err, result) {
+  if (err) throw err;
+  console.log(result);
+  start();
+}
+
+async function addDepartment() {
+  const department = await inquirer.prompt([
+    {
+      name: "name",
+      message: "What is the name of the department",
+    }
+  ]);
+
+  connection.query(
+    `insert into department (name) values ('${department.name}')`,
+    printResults
+  );
 }
 
 function addRole() {
-    connection.query ("select * from department", async function(err, results) {
-        const departments = results.map ( (result) => ({
-            name:result.name, 
-            value:result.id
-        }) )
-        const roleInfo = await inquirer.prompt([
-            {
-                name: "title",
-                message: "What is the title for the position"
-            },
-            {
-                name: "salary",
-                message: "What is the salary for the position"
-            },
-            {
-                type: "list",
-                name: "department_id",
-                message: "Which Department does the role belong to?",
-                choices:departments 
-            }
-        ])
-        connection.query (`insert into roles (title, salary, department_id) values('${roleInfo.title}','${roleInfo.salary}','${roleInfo.department_id}' )`, printResults)
-    })
+  connection.query("select * from department", async function (err, results) {
+    const departments = results.map((result) => ({
+      name: result.name,
+      value: result.id,
+    }));
+
+    const roleInfo = await inquirer.prompt([
+      {
+        name: "title",
+        message: "What is the title for the position",
+      },
+      {
+        name: "salary",
+        message: "What is the salary for the position",
+      },
+      {
+        type: "list",
+        name: "department_id",
+        message: "Which Department does the role belong to?",
+        choices: departments,
+      }
+    ]);
+
+    connection.query(
+      `insert into role (title, salary, department_id) values('${roleInfo.title}','${roleInfo.salary}','${roleInfo.department_id}' )`,
+      printResults
+    );
+  });
 }
 
 function addEmployee() {
-    connection.query ("select * from roles", async function(err, results) {
-        const roles = results.map ( (result) => ({
-            name:result.title, 
-            value:result.id
-        }) )
-        const employeeInfo = await inquirer.prompt([
-            {
-                name: "first_name",
-                message: "What is the first name of the employee"
-            },
-            {
-                name: "last_name",
-                message: "What is the last name of the employee"
-            },
-            {
-                type: "list",
-                name: "role_id",
-                message: "What is the employee's role?",
-                choices:roles 
-            }
-        ])
-        connection.query (`insert into employee (first_name, last_name, role_id) values('${employeeInfo.first_name}','${employeeInfo.last_name}','${employeeInfo.role_id}' )`, printResults)
-    })
+  connection.query("select * from role", async function (err, results) {
+    const roles = results.map((result) => ({
+      name: result.title,
+      value: result.id,
+    }));
+
+    const employeeInfo = await inquirer.prompt([
+      {
+        name: "first_name",
+        message: "What is the first name of the employee",
+      },
+      {
+        name: "last_name",
+        message: "What is the last name of the employee",
+      },
+      {
+        type: "list",
+        name: "role_id",
+        message: "What is the employee's role?",
+        choices: roles,
+      }
+    ]);
+
+    connection.query(
+      `insert into employee (first_name, last_name, role_id) values('${employeeInfo.first_name}','${employeeInfo.last_name}','${employeeInfo.role_id}' )`,
+      printResults
+    );
+  });
 }
 
-async function updateRole(){
-    connection.query ("select * from role", async function(err, results) {
-        const employee = results.map ( (result) => ({
-            name:result.first_name + result.last_name, 
-            value:result.id
-        }) )
-        const employeeInfo = await inquirer.prompt([
-            {
-                type: "list",
-                name: "employee_id",
-                message: "Which employee ID would you like to update?",
-                choices:employee
-            },
-            {
-                type: "list",
-                name: "role_id",
-                message: "Which employee ID would you like to update?",
-                choices:employee
-            }
-        ])
-    })
+function update_Employee() {
+  connection.query("select * from employee", (err, employees) => {
+    connection.query("select * from role", async function (err, roles) {
+      const roleChoices = roles.map((role) => ({
+        name: role.title,
+        value: role.id,
+      }));
+
+      const employeeChoices = employees.map((employee) => ({
+        name: employee.first_name + " " + employee.last_name,
+        value: employee.id,
+      }));
+
+      const updateEmployee = await inquirer.prompt([
+        {
+          type: "list",
+          name: "employee_id",
+          message: "Which employee would you like to update?",
+          choices: employeeChoices,
+        },
+        {
+          type: "list",
+          name: "role_id",
+          message: "What would you like their new role to be?",
+          choices: roleChoices,
+        },
+      ]);
+
+      connection.query(
+        `update employee set role_id=${updateEmployee.role_id} where id=${updateEmployee.employee_id}`,
+        printResults
+      );
+    });
+  });
 }
--- mysql.sql
+++ mysql.sql
@@ -1,44 +1,24 @@
-drop database if exists employee_tracker;
-create database employee_tracker;
-use employee_tracker;
+-- DROP DATABASE IF EXISTS employees;
 
-create table department(
- id int primary key auto_increment,
- name_dept varchar(30) not null);
- 
- insert into department(name_dept)
- values("Foods"), ("Sports"), ("Health"), ("Fashions");
- 
- select * from department;
- 
- create table roles(
- id int primary key auto_increment,
- title varchar(30) not null,
- salary decimal (10,2) not null,
- department_id int);
- 
- insert into roles(title, salary, department_id)
- values("Cook", 80000, 1), ("Director of Sports", 120000, 2), ("Nurse", 55000, 3), ("Public Relations", 100000, 4);
- 
- select * from roles;
- 
- 
- create table employees(
- id int primary key auto_increment,
- first_name varchar(30) not null,
- last_name varchar(30) not null,
- role_id int not null);
+-- CREATE DATABASE employees;
 
- 
- insert into employees(first_name, last_name, role_id)
- values("Tom", "Jerry", 1), ("Bill", "Belichick", 2), ("Tom","Brady", 3), ("Kevin", "Ferg", 4);
- 
- select * from employees;
- 
-select title, salary, name from roles 
-inner join department on role.department_id=department.id;
+-- USE employees;
 
-select first_name, last_name, title, salary, name from employees 
-inner join role on employee.role_id=roles.id 
-inner join department on roles.department_id=department.id;
+-- CREATE TABLE department (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE role (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(30) NOT NULL,
+--     salary DECIMAL (10,2) NOT NULL,
+--     department_id INT NOT NULL
+-- );
  
+-- CREATE TABLE employee (
+-- 	id INT AUTO_INCREMENT PRIMARY KEY,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     role_id INT NOT NULL
+-- );
